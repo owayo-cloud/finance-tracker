54eed2c64b19fe889f198917d4112fe4ac7c7b5a
owayo <owayopaul@gmail.com>
2025-10-19 19:05:49 +0300

    Add Page component for user profile with metadata

---
+++ frontend/app/dashboard/profile/page.tsx
@@ -0,0 +1,12 @@
+import AccountPage from ".";
+
+export const metadata = {
+  title: "My Profile - Finance Tracker",
+};
+
+
+export default function Page() {
+  return (
+    <AccountPage/>
+  );
+}

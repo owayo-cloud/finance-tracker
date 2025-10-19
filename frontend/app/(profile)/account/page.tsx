c66724004dfadc3f0e9fa8ca25babaad91c5a27e
owayo <owayopaul@gmail.com>
2025-10-19 09:28:52 +0300

    Add Page component for user account display

---
+++ frontend/app/(profile)/account/page.tsx
@@ -0,0 +1,12 @@
+import AccountPage from ".";
+
+export const metadata = {
+  title: "My Account - Finance Tracker",
+};
+
+
+export default function Page() {
+  return (
+    <AccountPage/>
+  );
+}

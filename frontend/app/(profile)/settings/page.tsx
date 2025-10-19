e1377f5d2c8e114ac4003355b2f844fb11cde571
owayo <owayopaul@gmail.com>
2025-10-19 19:04:01 +0300

    Add Page component for settings with metadata

---
+++ frontend/app/(profile)/settings/page.tsx
@@ -0,0 +1,12 @@
+import SettingsPage from ".";
+
+export const metadata = {
+  title: "Settings - Finance Tracker",
+};
+
+
+export default function Page() {
+  return (
+    <SettingsPage/>
+  );
+}
